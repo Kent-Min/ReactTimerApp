5f282cf28176948643b42489998af2c41e497490
Kent <minwenyan@gmail.com>
2016-08-17 11:30:09 -0500

    added count down form component and test

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -16,10 +16,9 @@ var Navigation = require('Navigation');
 var Main = (props) => {
   return (
     <div>
-      <div>
-        <div>
-          <Navigation/>
-          <p>Mian.jsx render</p>
+        <Navigation/>
+        <div className="row">
+          <div className="column small-centered medium-6 large-6">
             {props.children}
         </div>
 
